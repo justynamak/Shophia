3fb2e2c2842b1a891c9358762d1321e3ae1b907f
justynaMak <justynamakuch@outlook.com>
2019-12-17 14:50:04 +0100

    dodanie obsługi controls w sliderze

--- src/js/components/Slider.js
+++ src/js/components/Slider.js
@@ -5,8 +5,12 @@ const Slider = (function() {
     constructor() {
       this.maxSize = document.querySelectorAll(".slider__image").length;
       this.items = [...document.querySelectorAll(".slider__image")];
-      this.startShowImages();
+      this.controls = [...document.querySelectorAll(".slider__control")];
+      this.startSlider();
       this.waiting = 50000;
+      this.controls.forEach(control =>
+        control.addEventListener("click", this.handleClickControl.bind(this))
+      );
     }
     get activeSlide() {
       return _activeSlide;
@@ -19,31 +23,56 @@ const Slider = (function() {
       }
     }
 
-    takeCurrentElement() {
+    takeCurrentImage() {
       return document.querySelector(`#slide${_activeSlide}`);
+    }
+    takeCurrentControl() {
+      return document.querySelector(`#control${_activeSlide}`);
     }
     resetToStartSlide() {
       _activeSlide = 1;
     }
     showNextImage() {
       _activeSlide = ++_activeSlide;
+    }
+    showImage() {
+      this.idInterval ? this.showNextImage() : this.startSlider();
+
       if (this.activeSlide > this.maxSize) {
         this.resetToStartSlide();
       }
-      this.resetActiveClass();
-      this.takeCurrentElement().classList.add("slider__image--active");
+      this.resetActiveClass(this.items, "slider__image--active");
+      this.resetActiveClass(this.controls, "slider__control--active");
+
+      this.addActiveClass(this.takeCurrentImage(), "slider__image--active");
+      this.addActiveClass(this.takeCurrentControl(), "slider__control--active");
     }
-    startShowImages() {
-      setInterval(() => {
-        this.showNextImage();
-      }, 4000);
-    }
-    resetActiveClass() {
-      const notActiveSlides = this.items
+
+    resetActiveClass(arr, className) {
+      const notActiveSlides = arr
         .filter(item => item.id !== _activeSlide)
         .forEach(element => {
-          element.classList.remove("slider__image--active");
+          element.classList.remove(className);
         });
+    }
+    addActiveClass(element, className) {
+      element.classList.add(className);
+      return element;
+    }
+    handleClickControl(e) {
+      this.stopSlider();
+      const id = e.target.id.slice(-1);
+      _activeSlide = id;
+      this.showImage();
+    }
+    startSlider() {
+      this.idInterval = setInterval(() => {
+        this.showImage();
+      }, 4000);
+    }
+    stopSlider() {
+      clearInterval(this.idInterval);
+      this.idInterval = null;
     }
   }
   return mainSlider;
